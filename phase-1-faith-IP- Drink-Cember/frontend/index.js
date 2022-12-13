10694594f7175e65ecb5bfbd1dd0c8ccf193e32f
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-12-13 15:13:39 +0300

    created directories

---
+++ phase-1-faith-IP- Drink-Cember/frontend/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+})
+
+const createMenu=()=>{
+    const image = document.createElement("img");
+    const menu = document.querySelector(".cocktail-menu");
+    image.attributes("src", "cocktail.strDrinkThumb");
+    menu.appendChild("image");
+    
+    return menu
+}
+
+let 
